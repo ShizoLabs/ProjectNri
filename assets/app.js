c1b142fb07e59c6ec6c8509d35160126c075b827
Isafux <imperor178@mail.ru>
2026-01-28 22:37:18 +0300

    modal

--- assets/app.js
+++ assets/app.js
@@ -7,5 +7,24 @@
 import './styles/app.css';
 
 $(document).ready(function() {
+const dialog = document.querySelector('dialog');
+let isDragging = false;
+let offset = { x: 0, y: 0 };
 
+dialog.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    offset.x = e.clientX - dialog.getBoundingClientRect().left;
+    offset.y = e.clientY - dialog.getBoundingClientRect().top;
+});
+
+document.addEventListener('mousemove', (e) => {
+    if (!isDragging) return;
+    dialog.style.left = (e.clientX - offset.x) + 'px';
+    dialog.style.top = (e.clientY - offset.y) + 'px';
+    dialog.style.margin = '0';
+});
+
+document.addEventListener('mouseup', () => {
+    isDragging = false;
+});
 });
